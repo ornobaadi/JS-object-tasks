5124705bbf9e0a37cd45c78b4ebe450365e1714b
ornobaadi <aadi4789@gmail.com>
2024-09-19 16:14:23 +0600

    completed

---
+++ task5.js
@@ -0,0 +1,11 @@
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+
+for (const key in myObject){
+    console.log("Key: " + key + "| Type: " + typeof myObject[key]);
+}
